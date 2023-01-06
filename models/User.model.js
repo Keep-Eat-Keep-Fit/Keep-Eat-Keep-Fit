c183e3953b15c170c7d0eb821b20adea278bd47d
Luluyan2022 <emmymagao@gmail.com>
2023-01-06 23:45:00 +0100

    The basic setting of user-auth and profile page Co-authored-by: Xavier Chang <lavender880331@gmail.com>

--- models/User.model.js
+++ models/User.model.js
@@ -2,17 +2,23 @@ const { Schema, model } = require("mongoose");
 
 // TODO: Please make sure you edit the User model to whatever makes sense in this case
 const userSchema = new Schema(
-  {    
+  { 
+    username:{
+      type: String,
+      required: [true, 'Username is required.'],
+      unique: true,     
+      trim: true
+    },   
     email: {
       type: String,
-      required: true,
+      required: [true, 'Email is required.'],
       unique: true,
       lowercase: true,
       trim: true
     },
-    password: {
+    passwordHash: {
       type: String,
-      required: true
+      required: [true, 'Password is required.']
     },
     birthdate:{
       type: String,
